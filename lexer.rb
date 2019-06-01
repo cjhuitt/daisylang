44544dc3fa5cd15366b32bd064d38696f77a13ac
Caleb Huitt <development-caleb@huitt.org>
2019-06-01 13:54:13 -0500

    Add debugging capability

--- lexer.rb
+++ lexer.rb
@@ -1,8 +1,13 @@
 class Lexer
+  def initialize(debug=false)
+    @debug = debug
+  end
+
   def tokenize(code)
     blocklevel = 0
     tokens = []
     code.lines.each do |line|
+      debug_out("Tokenizing line >>#{line}<<")
       indent = line[/\A */].size
       blocks = indent / 4
       if blocks == blocklevel + 1
@@ -30,6 +35,7 @@ class Lexer
 
       tokens = []
       line.split.each do |chunk|
+        debug_out("Tokenizing chunk >>#{chunk}<<")
         tokens += tokenize_chunk(chunk)
         tokens << [:WHITESPACE, " "]
       end
@@ -45,25 +51,35 @@ class Lexer
       i = 0
       while i < chunk.size
         sub = chunk[i..-1]
+        debug_out("Checking partial >>#{sub}<<")
         if integer = sub[/\A(\d+)/, 1]
           tokens << [:INTEGER, integer.to_i]
           i += integer.size
+          debug_out("Extracted #{integer} (Integer)")
         elsif str = sub[/\A"(.*)"/, 1]
           tokens << [:STRING, str]
           i += str.size + 2
+          debug_out("Extracted \"#{str}\" (String)")
         elsif identifier = sub[/\A(\w+)/, 1]
           if KEYWORDS.include? identifier
             tokens << [identifier.upcase.to_sym, identifier]
+            debug_out("Extracted #{identifier} (Keyword)")
           else
             tokens << [:IDENTIFIER, identifier]
+            debug_out("Extracted #{identifier} (Identifier)")
           end
           i += identifier.size
         elsif op = sub[/\A([:()=+-\/^*])/, 1]
           tokens << [op, op]
           i += 1
+          debug_out("Extracted #{op} (Operator)")
         end
       end
 
       tokens
+    end
+
+    def debug_out(message)
+      puts message if @debug
     end
 end
