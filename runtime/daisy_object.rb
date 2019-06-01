fda8e14dda68aa5b84c20b31eb6b500dc8e8ccb0
Caleb Huitt <development-caleb@huitt.org>
2019-06-01 16:35:25 -0500

    Add some more bootstrapping and raise an error on an undefined message

--- runtime/daisy_object.rb
+++ runtime/daisy_object.rb
@@ -1,6 +1,21 @@
 # The base object everything inherits from
 class DaisyObject
-  def call(method, args)
-    puts( "Should be calling a method now" )
+  attr_accessor :runtime_class
+
+  def initialize(runtime_class)
+    @runtime_class = runtime_class
+  end
+
+  def call(message, args)
+    unknown_message(message, args)
+  end
+
+  def unknown_message(message, args)
+    # Todo: This should error-handle via Daisy code paths, not Ruby
+    raise "Received unknown message #{message} for #{@runtime_class}"
+  end
+
+  def new
+    DaisyObject.new(self)
   end
 end
