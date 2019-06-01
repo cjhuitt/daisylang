2f8182e008879d6cea951979010fd6da9a8f704e
Caleb Huitt <development-caleb@huitt.org>
2019-06-01 17:09:02 -0500

    More scaffolding for function lookup

--- runtime/daisy_object.rb
+++ runtime/daisy_object.rb
@@ -1,26 +1,22 @@
 # The base object everything inherits from
 class DaisyObject
-  attr_accessor :runtime_class, :runtime_methods
+  attr_accessor :runtime_class
 
   def initialize(runtime_class)
     @runtime_class = runtime_class
-    @runtime_methods = {}
   end
 
-  def call(message, args)
-    if method = runtime_methods[message]
-      method
-    else
-      unknown_message(message, args)
-    end
+  def dispatch(message, args)
+    method = @runtime_class.lookup(message)
+    return method.call(self, args) unless method.nil?
+
+    method = @runtime.class.lookup_dispatch() if method.nil?
+    method.call(self, message, args) unless method.nil?
+    unknown_message(message, args)
   end
 
   def unknown_message(message, args)
     # Todo: This should error-handle via Daisy code paths, not Ruby
     raise "Received unknown message #{message} for #{@runtime_class}"
-  end
-
-  def new
-    DaisyObject.new(self)
   end
 end
