0605521f54b6ba28b879102b127cabd207763db8
Caleb Huitt <development-caleb@huitt.org>
2019-06-01 17:19:32 -0500

    Add a string class and use it

--- runtime/bootstrap.rb
+++ runtime/bootstrap.rb
@@ -3,6 +3,7 @@ Constants = {}
 Constants["Class"] = DaisyClass.new
 Constants["Class"].runtime_class = Constants["Class"]
 Constants["Object"] = DaisyClass.new
+Constants["String"] = DaisyClass.new(Constants["Object"])
 
 root_self = Constants["Object"].new
 RootContext = Context.new(root_self)
