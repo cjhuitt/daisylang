92874fdb47fcad687ed40dad8b9c9a37c045f744
Caleb Huitt <development-caleb@huitt.org>
2019-05-31 22:07:53 -0500

    Add aspirational function parse

--- test/lexer_test.rb
+++ test/lexer_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 require "lexer"
 class LexerTest < Test::Unit::TestCase
-  def test_number
+  def test_int
     assert_equal [[:INTEGER, 1]], Lexer.new.tokenize("1")
+  end
+
+  def xtest_function
+    code = <<-CODE
+Function Integer Summation(n: Integer)
+    return n * (n - 1) / 2
+
+CODE
+  expected = [
+    [:FUNCTION, "Function"], [:WHITESPACE, " "], [:IDENTIFIER, "Integer"],
+        [:WHITESPACE, " "], [:IDENTIFIER, "Summation"], ['(', "("],
+        [:IDENTIFIER, "n"], [':', ":"], [:WHITESPACE, " "],
+        [:IDENTIFIER, "Integer"], [')', ")"], [:NEWLINE, "\n"],
+    [:BLOCKSTART, 1],
+      [:RETURN, "return"], [:WHITESPACE, " "], [:IDENTIFIER, "n"],
+        [:WHITESPACE, " "], ['*', "*"], [:WHITESPACE, " "],
+        ['(', "("], [:IDENTIFIER, "n"], [:WHITESPACE, " "],
+        ['-', "-"], [:INTEGER, 1], [')', ")"], [:WHITESPACE, " "],
+        ['/', "/"], [:WHITESPACE, " "], [:INTEGER, 2],
+        [:NEWLINE, "\n"],
+    [:BLOCKEND, 1], [:NEWLINE, "\n"]
+  ]
+
   end
 end
