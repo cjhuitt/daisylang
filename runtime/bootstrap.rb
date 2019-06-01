025974aed8fdff6d02e07c10d3b841d57c867c2b
Caleb Huitt <development-caleb@huitt.org>
2019-06-01 17:42:02 -0500

    Interpret integers and bootstrap addition for them

--- runtime/bootstrap.rb
+++ runtime/bootstrap.rb
@@ -4,6 +4,7 @@ Constants["Class"] = DaisyClass.new
 Constants["Class"].runtime_class = Constants["Class"]
 Constants["Object"] = DaisyClass.new
 Constants["String"] = DaisyClass.new(Constants["Object"])
+Constants["Integer"] = DaisyClass.new(Constants["Object"])
 
 root_self = Constants["Object"].new
 RootContext = Context.new(root_self)
@@ -11,4 +12,9 @@ RootContext = Context.new(root_self)
 Constants["Object"].def :print do |receiver, args|
   message = args.map { |arg| arg[1].ruby_value }
   puts message
+end
+
+Constants["Integer"].def :+ do |receiver, args|
+  result = receiver.ruby_value + args.first[1].ruby_value
+  Constants["Integer"].new(result)
 end
