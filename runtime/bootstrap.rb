3594d9951ce19d923950291e8b93fb4489585928
Caleb Huitt <development-caleb@huitt.org>
2019-06-02 13:18:48 -0500

    Rearrange the bootstrap to group types more closely together, and add the built-in known types to the root context's known types.

--- runtime/bootstrap.rb
+++ runtime/bootstrap.rb
@@ -2,21 +2,27 @@ Constants = {}
 
 Constants["Class"] = DaisyClass.new
 Constants["Class"].runtime_class = Constants["Class"]
+
 Constants["Object"] = DaisyClass.new
-Constants["String"] = DaisyClass.new(Constants["Object"])
-Constants["None"] = DaisyClass.new(Constants["Object"])
-
-Constants["none"] = Constants["None"].new(nil)
-
-root_self = Constants["Object"].new
-RootContext = Context.new(root_self)
-
 Constants["Object"].def :print do |receiver, args|
   message = args.map { |arg| arg[1].ruby_value }
   puts message
 end
 
+root_self = Constants["Object"].new
+RootContext = Context.new(root_self)
+
+RootContext.defined_types["Object"] = Constants["Object"]
+RootContext.defined_types["Class"] = Constants["Class"]
+
+Constants["String"] = DaisyClass.new(Constants["Object"])
+RootContext.defined_types["String"] = Constants["String"]
+
+Constants["None"] = DaisyClass.new(Constants["Object"])
+RootContext.defined_types["None"] = Constants["None"]
 Constants["None"].def :print do |receiver, args|
   puts "(none)"
 end
+
+Constants["none"] = Constants["None"].new(nil)
 
--- runtime/integer.rb
+++ runtime/integer.rb
@@ -1,4 +1,5 @@
 Constants["Integer"] = DaisyClass.new(Constants["Object"])
+RootContext.defined_types["Integer"] = Constants["Integer"]
 
 Constants["Integer"].def :+ do |receiver, args|
   result = receiver.ruby_value + args.first[1].ruby_value
