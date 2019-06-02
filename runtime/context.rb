11a3e80aba1ba1399f33c781f759b796f37bef6a
Caleb Huitt <development-caleb@huitt.org>
2019-06-02 13:16:51 -0500

    Remember the class type of the current self in the context, and defined types for the context

--- runtime/context.rb
+++ runtime/context.rb
@@ -1,7 +1,10 @@
 class Context
-  attr_reader :current_self
+  attr_reader :current_self, :current_class
+  attr_accessor :defined_types
 
-  def initialize(current_self)
+  def initialize(current_self, current_class=current_self.runtime_class)
     @current_self = current_self
+    @current_class = current_class
+    @defined_types = {}
   end
 end
