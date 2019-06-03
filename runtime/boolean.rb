ed2a9a41e0a7f00aaf6cf0e1ed746b202f2b6177
Caleb Huitt <development-caleb@huitt.org>
2019-06-02 20:28:59 -0500

    Handle boolean inequality operators

--- runtime/boolean.rb
+++ runtime/boolean.rb
@@ -4,12 +4,21 @@ Constants["true"] = Constants["Boolean"].new(true)
 Constants["false"] = Constants["Boolean"].new(false)
 
 Constants["Boolean"].def :== do |interpreter, receiver, args|
-  Constants["true"] if receiver.ruby_value == args.first[1].ruby_value
+  if receiver.ruby_value == args.first[1].ruby_value
+    Constants["true"]
+  else
+    Constants["false"]
+  end
 end
 
-#Constants["Boolean"].def :!= do |interpreter, receiver, args|
-#end
-#
+Constants["Boolean"].def :!= do |interpreter, receiver, args|
+  if receiver.ruby_value == args.first[1].ruby_value
+    Constants["false"]
+  else
+    Constants["true"]
+  end
+end
+
 #Constants["Boolean"].def :&& do |interpreter, receiver, args|
 #end
 #
