ea3e08318aa3dd7e7a884fd4df3d47c598993a60
Caleb Huitt <development-caleb@huitt.org>
2019-06-02 20:24:21 -0500

    Handle boolean equality operators

--- runtime/boolean.rb
+++ runtime/boolean.rb
@@ -3,9 +3,9 @@ RootContext.defined_types["Boolean"] = Constants["Boolean"]
 Constants["true"] = Constants["Boolean"].new(true)
 Constants["false"] = Constants["Boolean"].new(false)
 
-#Constants["Boolean"].def :== do |interpreter, receiver, args|
-#  Constants["true"] if receiver.ruby_value == args.first[1].ruby_value
-#end
+Constants["Boolean"].def :== do |interpreter, receiver, args|
+  Constants["true"] if receiver.ruby_value == args.first[1].ruby_value
+end
 
 #Constants["Boolean"].def :!= do |interpreter, receiver, args|
 #end
