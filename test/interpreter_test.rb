1aabf1a0a3bee8166bf624c46d303da2e3cfee11
Caleb Huitt <development-caleb@huitt.org>
2019-06-14 05:53:20 -0500

    Extract setup code

--- test/interpreter_test.rb
+++ test/interpreter_test.rb
@@ -1,29 +1,16 @@
 require "test_helper"
 require "runtime/interpreter"
 
-class TestNode
-  attr_accessor :visited
-
-  def initialize
-    @visited = false
-  end
-
-  include Visitable
-end
-
-class InterpreterMock < Interpreter
-  def visit_TestNode(node)
-    node.visited = true
-  end
-end
-
 class DaisyInterpreterTest < Test::Unit::TestCase
+  def setup
+    @interpreter = Interpreter.new
+  end
+
   def test_can_define_variable
-    interpreter = Interpreter.new
-    interpreter.eval("asdf = true")
-    symbol = interpreter.context.symbol("asdf")
+    @interpreter = Interpreter.new
+    @interpreter.eval("asdf = true")
+    symbol = @interpreter.context.symbol("asdf")
     assert_equal Constants["true"], symbol
     assert_equal Constants["Boolean"], symbol.runtime_class
   end
-
 end
