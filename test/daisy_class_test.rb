ae514b0e7e2a68e71fb22844ba9d53388752c66b
Caleb Huitt <development-caleb@huitt.org>
2019-06-13 19:18:02 -0500

    Add tests for looking up methods

--- test/daisy_class_test.rb
+++ test/daisy_class_test.rb
@@ -18,4 +18,42 @@ class DaisyClassTest < Test::Unit::TestCase
     class2 = DaisyClass.new("Foo")
     assert_false class2.is_type(daisy_class)
   end
+
+  def test_lookup_finds_defined_method
+    daisy_class = DaisyClass.new("Test")
+    run = false
+    daisy_class.def :foo do |passed_interpreter, passed_receiver, passed_args|
+      run = true
+    end
+    method = daisy_class.lookup("foo")
+    assert_not_nil method
+    assert_false run
+    method.call("1", "2", "3")
+    assert_true run
+  end
+
+  def test_lookup_finds_method_defined_in_superclass
+    daisy_class = DaisyClass.new("Test")
+    run = false
+    daisy_class.def :foo do |passed_interpreter, passed_receiver, passed_args|
+      run = true
+    end
+    class2 = DaisyClass.new("Foo", daisy_class)
+    method = class2.lookup("foo")
+    assert_not_nil method
+    assert_false run
+    method.call("1", "2", "3")
+    assert_true run
+  end
+
+  def test_lookup_does_not_find_method_from_different_class
+    daisy_class = DaisyClass.new("Test")
+    run = false
+    daisy_class.def :foo do |passed_interpreter, passed_receiver, passed_args|
+      run = true
+    end
+    class2 = DaisyClass.new("Foo")
+    method = class2.lookup("foo")
+    assert_nil method
+  end
 end
