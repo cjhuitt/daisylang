6fef9fb2b0e71777608d302d8dc96f7c3f1e85dc
Caleb Huitt <development-caleb@huitt.org>
2019-06-13 21:28:47 -0500

    Tests for retrieving definitions and symbols

--- test/context_test.rb
+++ test/context_test.rb
@@ -10,4 +10,33 @@ class DaisyContextTest < Test::Unit::TestCase
     assert_equal interpreter, context.interpreter
   end
 
+  def test_finds_definition
+    new_self = Constants["Object"].new
+    context = Context.new(nil, new_self)
+    context.assign_symbol("foo", "foo")
+    assert_equal "foo", context.definition_of("foo")
+  end
+
+  def test_finds_definition_in_previous_context
+    new_self = Constants["Object"].new
+    prev_context = Context.new(nil, new_self)
+    prev_context.assign_symbol("foo", "foo")
+    context = Context.new(prev_context, new_self)
+    assert_equal "foo", context.definition_of("foo")
+  end
+
+  def test_finds_symbol
+    new_self = Constants["Object"].new
+    context = Context.new(nil, new_self)
+    context.assign_symbol("foo", "foo")
+    assert_equal "foo", context.symbol("foo")
+  end
+
+  def test_finds_symbol_in_previous_context
+    new_self = Constants["Object"].new
+    prev_context = Context.new(nil, new_self)
+    prev_context.assign_symbol("foo", "foo")
+    context = Context.new(prev_context, new_self)
+    assert_equal "foo", context.symbol("foo")
+  end
 end
