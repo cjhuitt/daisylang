d50d4bd5dc284b46627a21a7731f47d155a70e7c
Caleb Huitt <development-caleb@huitt.org>
2019-06-13 20:15:21 -0500

    Test that the true/false predefines are constants

--- test/boolean_test.rb
+++ test/boolean_test.rb
@@ -2,9 +2,13 @@ require "test_helper"
 require "runtime/runtime"
 
 class DaisyBooleanTest < Test::Unit::TestCase
-  def test_in_root_context
+  def test_class_in_root_context
     assert_not_nil RootContext.symbol("Boolean")
   end
 
+  def test_constants_for_only_values
+    assert_not_nil Constants.key?("true")
+    assert_not_nil Constants.key?("false")
+  end
 end
 
