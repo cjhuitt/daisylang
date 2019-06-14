bb7be5a87abcbd03137a2729410a7a635ff97e3b
Caleb Huitt <development-caleb@huitt.org>
2019-06-13 19:09:45 -0500

    Assert function dispatch raises error if no function found.

--- test/daisy_object_test.rb
+++ test/daisy_object_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 require "runtime/runtime"
 
 class DaisyObjectTest < Test::Unit::TestCase
-  def test_dispatch_with_existing_function
+  def test_dispatch_works_with_existing_method
     daisy_class = DaisyClass.new("Test")
     ran = false
     context = Context.new(nil, nil, nil)
@@ -14,7 +14,20 @@ class DaisyObjectTest < Test::Unit::TestCase
       assert_equal interpreter, passed_interpreter
       assert_equal daisy_object, passed_receiver
       assert_equal args, passed_args
+      ran = true
     end
     daisy_object.dispatch(context, "foo", args)
+    assert_true ran
+  end
+
+  def test_dispatch_throws_with_no_method
+    daisy_class = DaisyClass.new("Test")
+    context = Context.new(nil, nil, nil)
+    interpreter = 123456
+    context.interpreter = interpreter
+    daisy_object = DaisyObject.new(daisy_class)
+    assert_raise do
+      daisy_object.dispatch(context, "bar", args)
+    end
   end
 end
