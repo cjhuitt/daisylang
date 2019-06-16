8833fc39b8d2b17b3879b8b20d5080f73b62975b
Caleb Huitt <development-caleb@huitt.org>
2019-06-16 13:28:24 -0500

    Fix toString method for array

--- runtime/array.rb
+++ runtime/array.rb
@@ -3,12 +3,15 @@ RootContext.symbols["Array"] = Constants["Array"]
 
 Constants["Array"].add_contract(Constants["Stringifiable"].ruby_value)
 Constants["Array"].def :toString do |interpreter, receiver, args|
-  return Constants["String"].new("[]") if receiver.ruby_value.empty?
-  strings = receiver.ruby_value.map { |item|
-    stringify = Constants["Object"].lookup("to_str")
-    stringify.call(interpreter, item, args)
-  }
-  Constants["String"].new( "[#{strings.join(", ")}]" )
+  if receiver.ruby_value.empty?
+    Constants["String"].new("[]")
+  else
+    strings = receiver.ruby_value.map { |item|
+      stringify = Constants["Object"].lookup("to_str")
+      stringify.call(interpreter, item, args)
+    }
+    Constants["String"].new( "[#{strings.join(", ")}]" )
+  end
 end
 
 Constants["Array"].def :+ do |interpreter, receiver, args|
