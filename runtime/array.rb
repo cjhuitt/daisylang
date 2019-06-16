9ce09bb3ae1b2a5c83d52f83499b8549131c8f86
Caleb Huitt <development-caleb@huitt.org>
2019-06-16 13:20:32 -0500

    Add countable contract to array

--- runtime/array.rb
+++ runtime/array.rb
@@ -20,3 +20,11 @@ Constants["Array"].add_contract(Constants["Verifiable"].ruby_value)
 Constants["Array"].def :'?' do |interpreter, receiver, args|
   receiver.ruby_value.nil? || receiver.ruby_value.empty? ? Constants["false"] : Constants["true"]
 end
+
+Constants["Array"].add_contract(Constants["Countable"].ruby_value)
+Constants["Array"].def :empty? do |interpreter, receiver, args|
+  receiver.ruby_value.empty? ? Constants["true"] : Constants["false"]
+end
+Constants["Array"].def :count do |interpreter, receiver, args|
+  Constants["Integer"].new(receiver.ruby_value.count)
+end
--- test/array_test.rb
+++ test/array_test.rb
@@ -5,10 +5,6 @@ class DaisyArrayTest < Test::Unit::TestCase
   def test_class_in_root_context
     assert_not_nil RootContext.symbol("Array", nil)
   end
-#
-#  def test_logical_operations_exist
-#    assert_not_nil Constants["Array"].lookup("?")
-#  end
 
   def test_stringifiable
     assert_true Constants["Array"].has_contract(Constants["Stringifiable"].ruby_value)
@@ -24,10 +20,11 @@ class DaisyArrayTest < Test::Unit::TestCase
     assert_not_nil Constants["Array"].lookup("?")
   end
 
-#  def test_equatable
-#    assert_true Constants["Array"].has_contract(Constants["Equatable"].ruby_value)
-#    assert_not_nil Constants["Array"].lookup("==")
-#    assert_not_nil Constants["Array"].lookup("!=")
-#  end
+  def test_countable
+    assert_true Constants["Array"].has_contract(Constants["Countable"].ruby_value)
+    assert_not_nil Constants["Array"].lookup("empty?")
+    assert_not_nil Constants["Array"].lookup("count")
+  end
+
 end
 
