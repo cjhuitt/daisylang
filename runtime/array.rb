cc54f09a477353de55ab7ea6850111c7de59bfa0
Caleb Huitt <development-caleb@huitt.org>
2019-06-16 12:05:34 -0500

    Pretty-print arrays

--- runtime/array.rb
+++ runtime/array.rb
@@ -1,3 +1,12 @@
 Constants["Array"] = DaisyClass.new("Array", Constants["Object"])
 RootContext.symbols["Array"] = Constants["Array"]
 
+Constants["Array"].add_contract(Constants["Stringifiable"].ruby_value)
+Constants["Array"].def :toString do |interpreter, receiver, args|
+  return Constants["String"].new("[]") if receiver.ruby_value.empty?
+  strings = receiver.ruby_value.map { |item|
+    stringify = Constants["Object"].lookup("to_str")
+    stringify.call(interpreter, item, args)
+  }
+  Constants["String"].new( "[#{strings.join(", ")}]" )
+end
