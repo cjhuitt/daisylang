e70195911b436b87552cde6fc7ba611430d127be
Caleb Huitt <development-caleb@huitt.org>
2019-06-19 20:15:15 -0500

    Split the context and context manager classes into their own files

---
+++ runtime/context_manager.rb
@@ -0,0 +1,33 @@
+require 'context'
+
+class ContextManager
+  attr_accessor :context
+
+  def initialize(root_context)
+    @context = root_context
+    @context_queue = []
+  end
+
+  def enter_method_context(new_self)
+    @context_queue.push(@context)
+    @context = Context.new(@context, new_self)
+  end
+
+  def enter_class_definition_context(daisy_class)
+    @context_queue.push(@context)
+    @context = Context.new(@context, daisy_class, daisy_class)
+    @context.defining_class = daisy_class
+    @context
+  end
+
+  def enter_contract_definition_context(contract)
+    @context_queue.push(@context)
+    @context = Context.new(@context, contract, contract)
+  end
+
+  def leave_context(context)
+    raise "Leaving inactive context" if !context.nil? && context != @context
+    @context = @context_queue.pop()
+  end
+end
+
