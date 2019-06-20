d78eef1e4e2d0a695d7996b664806b95ca3c5ed0
Caleb Huitt <development-caleb@huitt.org>
2019-06-19 20:26:57 -0500

    More scope tests

--- test/context_manager_test.rb
+++ test/context_manager_test.rb
@@ -18,4 +18,22 @@ class DaisyContextManagerTest < Test::Unit::TestCase
     assert_nil @manager.context.symbol("foo", nil)
   end
 
+  def test_symbol_defined_in_root_scope_available_in_method_scope
+    method_context = @manager.enter_method_scope(@test_self)
+    assert_equal Constants["None"], @manager.context.symbol("None", nil)
+    @manager.leave_scope(method_context)
+  end
+
+  def test_symbol_defined_in_root_scope_available_in_class_definition_scope
+    method_context = @manager.enter_class_definition_scope(@test_self)
+    assert_equal Constants["None"], @manager.context.symbol("None", nil)
+    @manager.leave_scope(method_context)
+  end
+
+  def test_symbol_defined_in_root_scope_available_in_contract_definition_scope
+    method_context = @manager.enter_contract_definition_scope(@test_self)
+    assert_equal Constants["None"], @manager.context.symbol("None", nil)
+    @manager.leave_scope(method_context)
+  end
+
 end
