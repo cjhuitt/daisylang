52e0838c6293d06d0494c13c290cd462fa46f0ee
Caleb Huitt <development-caleb@huitt.org>
2019-06-19 20:40:35 -0500

    Add tests around symbol availability inside other scopes while in a method scope

--- test/context_manager_test.rb
+++ test/context_manager_test.rb
@@ -82,6 +82,32 @@ class DaisyContextManagerTest < Test::Unit::TestCase
     @manager.leave_scope(file_context)
   end
 
+  def test_symbol_defined_in_method_scope_available_in_flow_control_block_scope
+    method_context = @manager.enter_method_scope(@test_self)
+    flow_context = @manager.enter_flow_control_block_scope()
+    assert_equal Constants["None"], @manager.context.symbol("None", nil)
+    @manager.leave_scope(flow_context)
+    @manager.leave_scope(method_context)
+  end
+
+  def test_symbol_defined_in_method_scope_available_in_class_definition_scope
+    method_context = @manager.enter_method_scope(@test_self)
+    daisy_class = DaisyClass.new("Foo", Constants["Object"])
+    class_context = @manager.enter_class_definition_scope(daisy_class)
+    assert_equal Constants["None"], @manager.context.symbol("None", nil)
+    @manager.leave_scope(class_context)
+    @manager.leave_scope(method_context)
+  end
+
+  def test_symbol_defined_in_method_scope_available_in_contract_definition_scope
+    method_context = @manager.enter_method_scope(@test_self)
+    contract = DaisyContract.new("Foo")
+    contract_context = @manager.enter_contract_definition_scope(contract)
+    assert_equal Constants["None"], @manager.context.symbol("None", nil)
+    @manager.leave_scope(contract_context)
+    @manager.leave_scope(method_context)
+  end
+
 
   def test_symbol_defined_in_function_not_available_after_scope_is_left
     method_context = @manager.enter_method_scope(@test_self)
