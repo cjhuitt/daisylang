eb10a032ccfd29c9581d7d6f58f46893359d1b95
Caleb Huitt <development-caleb@huitt.org>
2019-06-19 20:35:56 -0500

    Move function

--- test/context_manager_test.rb
+++ test/context_manager_test.rb
@@ -11,13 +11,6 @@ class DaisyContextManagerTest < Test::Unit::TestCase
   def cleanup
   end
 
-  def test_symbol_defined_in_function_not_available_after_scope_is_left
-    method_context = @manager.enter_method_scope(@test_self)
-    method_context.assign_symbol("foo", nil, Constants["true"])
-    @manager.leave_scope(method_context)
-    assert_nil @manager.context.symbol("foo", nil)
-  end
-
   def test_symbol_defined_in_root_scope_available_in_file_scope
     file = Constants["Object"].new
     file_context = @manager.enter_file_scope(@test_self)
@@ -49,6 +42,13 @@ class DaisyContextManagerTest < Test::Unit::TestCase
     contract_context = @manager.enter_contract_definition_scope(contract)
     assert_equal Constants["None"], @manager.context.symbol("None", nil)
     @manager.leave_scope(contract_context)
+  end
+
+  def test_symbol_defined_in_function_not_available_after_scope_is_left
+    method_context = @manager.enter_method_scope(@test_self)
+    method_context.assign_symbol("foo", nil, Constants["true"])
+    @manager.leave_scope(method_context)
+    assert_nil @manager.context.symbol("foo", nil)
   end
 
 end
