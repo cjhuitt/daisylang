87574e67635bbc8c08fd084c4239f130afc43aa9
Caleb Huitt <development-caleb@huitt.org>
2019-06-22 10:38:43 -0500

    Add ability to get categry from an enum entry

--- test/daisy_enum_test.rb
+++ test/daisy_enum_test.rb
@@ -75,6 +75,7 @@ class DaisyEnumTest < Test::Unit::TestCase
   def test_entry_reflection
     assert_not_nil Constants["EnumEntry"].lookup("name")
     assert_not_nil Constants["EnumEntry"].lookup("value")
+    assert_not_nil Constants["EnumEntry"].lookup("category")
   end
 
 end
