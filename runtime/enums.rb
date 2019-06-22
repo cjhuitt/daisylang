606e13948e1923b7b18b53927faa8e00a0edf4c8
Caleb Huitt <development-caleb@huitt.org>
2019-06-22 10:27:25 -0500

    Make enum categories and entries printable

--- runtime/enums.rb
+++ runtime/enums.rb
@@ -1,6 +1,18 @@
 Constants["EnumCategory"] = DaisyClass.new("EnumCategory", Constants["Object"])
 RootContext.symbols["EnumCategory"] = Constants["EnumCategory"]
 
+Constants["EnumCategory"].add_contract(Constants["Stringifiable"].ruby_value)
+Constants["EnumCategory"].def :toString do |interpreter, receiver, args|
+  values = receiver.entries.keys.join(", ")
+  Constants["String"].new( "#{receiver.name} (#{values})" )
+end
+
+
 Constants["EnumEntry"] = DaisyClass.new("EnumEntry", Constants["Object"])
 RootContext.symbols["EnumEntry"] = Constants["EnumEntry"]
+
+Constants["EnumEntry"].add_contract(Constants["Stringifiable"].ruby_value)
+Constants["EnumEntry"].def :toString do |interpreter, receiver, args|
+  Constants["String"].new( "#{receiver.name}(#{receiver.value.ruby_value})" )
+end
 
