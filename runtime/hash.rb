2e0b1ea543092efa656698bca81c72494c14f30f
Caleb Huitt <development-caleb@huitt.org>
2019-06-22 21:10:02 -0500

    Round out the hash API some.

--- runtime/hash.rb
+++ runtime/hash.rb
@@ -14,3 +14,28 @@ Constants["Hash"].def :toString do |interpreter, receiver, args|
     Constants["String"].new( "{#{strings.join(", ")}}" )
   end
 end
+
+Constants["Hash"].def :+ do |interpreter, receiver, args|
+  result = receiver.ruby_value + args.first[1].ruby_value
+  Constants["Hash"].new(result)
+end
+
+Constants["Hash"].add_contract(Constants["Verifiable"].ruby_value)
+Constants["Hash"].def :'?' do |interpreter, receiver, args|
+  receiver.ruby_value.nil? || receiver.ruby_value.empty? ? Constants["false"] : Constants["true"]
+end
+
+Constants["Hash"].add_contract(Constants["Countable"].ruby_value)
+Constants["Hash"].def :empty? do |interpreter, receiver, args|
+  receiver.ruby_value.empty? ? Constants["true"] : Constants["false"]
+end
+Constants["Hash"].def :count do |interpreter, receiver, args|
+  Constants["Integer"].new(receiver.ruby_value.count)
+end
+
+Constants["Hash"].def :append! do |interpreter, receiver, args|
+  args.each do |arg|
+    receiver.ruby_value.update(arg[1].ruby_value)
+  end
+  receiver
+end
