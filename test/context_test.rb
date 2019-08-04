df0b9e7aa0e088d6ac395bed6b124840f5618cdf
Caleb Huitt <development-caleb@huitt.org>
2019-08-04 16:07:43 -0500

    Update test to not use the runtime_methods accessor

--- test/context_test.rb
+++ test/context_test.rb
@@ -30,7 +30,7 @@ class DaisyContextTest < Test::Unit::TestCase
     context = Context.new(nil, daisy_class, daisy_class)
     method = DaisyMethod.new("bar", Constants["None"], {}, NoneNode.new)
     context.add_method(method)
-    assert_true daisy_class.runtime_methods.key?("bar")
+    assert_false daisy_class.lookup("bar").nil?
   end
 
   def test_add_symbol_when_defining_class_adds_field_to_class
